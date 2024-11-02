425445f2a6cd3d2eb35c5d3749b84e13a198823c
johncfrauen-lc101 <christian.frauenhoffer.cs@launchcode.org>
2024-11-02 15:38:11 -0600

    updated exercise starter file

--- exercise-starter-file.js
+++ exercise-starter-file.js
@@ -1,39 +1,311 @@
-export function exercise_01() {}
+import { fileURLToPath } from "url";
 
-export function exercise_02() {}
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  // To run the code you write for each exercise, change the text below to match the name
+  // of the exercise.
+  // For Example:
+  // If I want to run exercise_05 below, I would change the text from "exercise_01" to
+  // "exercise_05", save this file, and then when I run this file with `node exercise.js`
+  // the code for exercise_05 will run
+  exercise_01();
+}
 
-export function exercise_03() {}
+export function exercise_01() {
+  /* 
+   
+    Exercise 1
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_04() {}
+export function exercise_02() {
+  /* 
+   
+    Exercise 2
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_05() {}
+export function exercise_03() {
+  /* 
+   
+    Exercise 3
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_06() {}
+export function exercise_04() {
+  /* 
+   
+    Exercise 4
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_07() {}
+export function exercise_05() {
+  /* 
+   
+    Exercise 5
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_08() {}
+export function exercise_06() {
+  /* 
+   
+    Exercise 6
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_09() {}
+export function exercise_07() {
+  /* 
+   
+    Exercise 7
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_10() {}
+export function exercise_08() {
+  /* 
+   
+    Exercise 8
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_11() {}
+export function exercise_09() {
+  /* 
+   
+    Exercise 9
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_12() {}
+export function exercise_10() {
+  /* 
+   
+    Exercise 10
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_13() {}
+export function exercise_11() {
+  /* 
+   
+    Exercise 11
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_14() {}
+export function exercise_12() {
+  /* 
+   
+    Exercise 12
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_15() {}
+export function exercise_13() {
+  /* 
+   
+    Exercise 13
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_16() {}
+export function exercise_14() {
+  /* 
+   
+    Exercise 14
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_17() {}
+export function exercise_15() {
+  /* 
+   
+    Exercise 15
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_18() {}
+export function exercise_16() {
+  /* 
+   
+    Exercise 16
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_19() {}
+export function exercise_17() {
+  /* 
+   
+    Exercise 17
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
 
-export function exercise_20() {}
+export function exercise_18() {
+  /* 
+   
+    Exercise 18
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
+
+export function exercise_19() {
+  /* 
+   
+    Exercise 19
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
+
+export function exercise_20() {
+  /* 
+   
+    Exercise 20
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
+
+export function exercise_21() {
+  /* 
+   
+    Exercise 21
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
+
+export function exercise_22() {
+  /* 
+   
+    Exercise 22
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
+
+export function exercise_23() {
+  /* 
+   
+    Exercise 23
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
+
+export function exercise_24() {
+  /* 
+   
+    Exercise 24
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
+
+export function exercise_25() {
+  /* 
+   
+    Exercise 25
+    
+    
+  
+  */
+  // CODE IN THE OPEN LINES BELOW
+  // Enter your code in the empty lines ABOVE
+}
